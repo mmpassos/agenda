b27041947cb82e2d6b0991d83e570204c0aaa240
mmpassos <mauriciomspassos@gmail.com>
2017-05-02 03:17:38 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <windows.h>
@@ -9,16 +8,23 @@
 #define MAX_CHAR_NOME_EVENTO 80
 #define MAX_CHAR_BUSCA 30
 
+
+
 #define DIREITA 1
 #define BAIXO 2
 #define CIMA 4
 #define ESQUERDA 3
 
+
+
+
+
 // ============= funçoes para interface ============================
-typedef struct{
-	COORD lsup;
-	COORD linf;
-	char * texto_salvo;
+
+typedef struct {
+    COORD lsup;
+    COORD linf;
+    char * texto_salvo;
 }bloco_tela;
 
 /*
@@ -27,31 +33,33 @@ bloco_tela gravaTela(COORD lsup, COORD linf){
     ret.lsup = lsup;
     ret.linf = linf;
     ret.texto_salvo = malloc((linf.X-lsup.X+1)*(linf.Y-lsup.Y+1));
-
     for (i=0;i<ncarac;i++)
         texto[i] = get_char_at_xy(inicio.X+i,inicio.Y);
         texto[i]='\0';
 }
 */
 
-char get_char_at_xy(int x, int y){
- 	CHAR_INFO ci;
-  	COORD xy = { 0, 0 };
-  	SMALL_RECT rect = { x, y, x, y };
-  	return ReadConsoleOutput( GetStdHandle( STD_OUTPUT_HANDLE ), &ci, (COORD){1,1}, xy, &rect )
-       						  ? ci.Char.AsciiChar : '\0';
+
+char get_char_at_xy( int x, int y )
+  {
+  CHAR_INFO ci;
+  COORD xy = { 0, 0 };
+  SMALL_RECT rect = { x, y, x, y };
+  return ReadConsoleOutput( GetStdHandle( STD_OUTPUT_HANDLE ), &ci, (COORD){1,1}, xy, &rect )
+       ? ci.Char.AsciiChar
+       : '\0';
   }
 
 void gotoxy(int x, int y){
-	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),(COORD){x,y});
+     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),(COORD){x,y});
 }
 
-void escreveTexto(char *texto, COORD inicio, int tam_linha){
-    int i = 0, j = 0;
+void escreveTexto(char * texto, COORD inicio, int tam_linha){
+    int i=0,j=0;
     gotoxy(inicio.X, inicio.Y);
-    for(;texto[i]!='\0'; i++, j++){
+    for(;texto[i]!='\0';i++,j++){
         if(j==tam_linha){
-        	j=0;
+            j=0;
             inicio.Y++;
             gotoxy(inicio.X,inicio.Y);
         }
@@ -92,8 +100,9 @@ void entra_texto(COORD inicio, int tam, char *texto){
     }
 
 }
-
 // =================================== FUNÇOES DE SELECAO =========================================
+
+
 typedef struct{
     char carac[3];
     COORD origem;
@@ -106,38 +115,35 @@ typedef struct{
 
 void desenha_cursor(mCursor curs){
     gotoxy( curs.origem.X + curs.offset.X * curs.mn.X, curs.origem.Y + curs.offset.Y * curs.mn.Y);
-  		putchar(curs.carac[0]);
+    putchar(curs.carac[0]);
     gotoxy(curs.origem.X + curs.offset.X * curs.mn.X + curs.dist, curs.origem.Y + curs.offset.Y * curs.mn.Y);
-    	putchar(curs.carac[1]);
+    putchar(curs.carac[1]);
 }
 
 void apaga_cursor(mCursor curs){
     gotoxy( curs.origem.X + curs.offset.X * curs.mn.X, curs.origem.Y + curs.offset.Y * curs.mn.Y);
-    	putchar(' ');
+    putchar(' ');
     gotoxy(curs.origem.X + curs.offset.X * curs.mn.X + curs.dist, curs.origem.Y + curs.offset.Y * curs.mn.Y);
-    	putchar(' ');
+    putchar(' ');
 }
 
+
 COORD move_cursor(mCursor *curs,int direcao){
-	switch (direcao){
-		case DIREITA:
-		if(curs->limites.X>curs->mn.X) 
-			curs->mn.X++;
-		break;
-		case ESQUERDA:
-		if(0<curs->mn.X) 
-			curs->mn.X--;
-		break;
-		case CIMA:
-		if(0<curs->mn.Y) 
-			curs->mn.Y--;
-		break;
-		case BAIXO:
-		if(curs->limites.Y>curs->mn.Y) 
-			curs->mn.Y++;
-		break;
-	}
-	return curs->mn;
+switch (direcao){
+    case DIREITA:
+        if(curs->limites.X>curs->mn.X) curs->mn.X++;
+        break;
+    case ESQUERDA:
+        if(0<curs->mn.X) curs->mn.X--;
+        break;
+    case CIMA:
+        if(0<curs->mn.Y) curs->mn.Y--;
+        break;
+    case BAIXO:
+        if(curs->limites.Y>curs->mn.Y) curs->mn.Y++;
+        break;
+    }
+    return curs->mn;
 }
 
 void criaCursor(mCursor* curs, int dist, COORD origem, COORD limites, COORD offset){
@@ -150,28 +156,31 @@ void criaCursor(mCursor* curs, int dist, COORD origem, COORD limites, COORD offs
     curs->mn = (COORD){0,0};
 }
 
+
 // ============================= Funcoes de data ===================================================
+
 typedef struct tm tm;
 
-int numero_dias_mes(int mes, int ano){
-	int dias;
-        if (mes == 4 || mes == 6 || mes == 9 || mes == 11)
-        	dias = 30;
-        else if (mes == 2){ 
-        	int ehBissexto = (ano % 4 == 0 && ano % 100 != 0) || (ano % 400 == 0);
-            if (ehBissexto)
-      	    	dias = 29;
-            else
+
+int numero_dias_mes(int mes , int ano){
+            int dias;
+            if (mes == 4 || mes == 6 || mes == 9 || mes == 11)
+              dias = 30;
+            else if (mes == 2)
+            { int ehBissexto = (ano % 4 == 0 && ano % 100 != 0) || (ano % 400 == 0);
+              if (ehBissexto)
+                dias = 29;
+              else
                 dias = 28;
-            }else
-           		 dias = 31;
-    return dias;
+            }
+            else
+              dias = 31;
+            return dias;
 }
 
 int valida_data(tm data){
     if(numero_dias_mes(data.tm_mon,data.tm_year) < data.tm_mday || data.tm_mday < 1 || data.tm_mon > 12 || data.tm_mon < 1)return 0;
     if(data.tm_hour <0 || data.tm_hour > 23 || data.tm_min < 0 || data.tm_min> 59)return 0;
-
     return 1;
 }
 
@@ -179,28 +188,40 @@ int dia_semana(int d, int m , int a){
     return (d += m < 3 ? a-- : a - 2, 23*m/9 + d + 4 + a/4- a/100 + a/400)%7;
 }
 
+
 void desenha_calendario(int mes, int ano,COORD origem){
-    gotoxy(origem.X,origem.Y);
-    	printf("D    S    T    Q    Q    S    S    ");
-    origem.Y++;
-    gotoxy(origem.X,origem.Y);
-    int dia_1 = dia_semana(1,mes,ano);
-    int tam_mes = numero_dias_mes(mes,ano);
-    int i,j=0;
-    for(i=0;i<dia_1;i++,j++)
-    	printf("     ");
+        gotoxy(origem.X,origem.Y);
+        printf("D    S    T    Q    Q    S    S    ");
+        origem.Y++;
+        gotoxy(origem.X,origem.Y);
+        int dia_1 = dia_semana(1,mes,ano);
+        int tam_mes = numero_dias_mes(mes,ano);
+        int i,j=0;
+        for(i=0;i<dia_1;i++,j++)
+            printf("     ");
         for(i=1;i<=tam_mes;i++,j++){
             if(j==7){
                 origem.Y+=2;
                 gotoxy(origem.X,origem.Y);
-        	        j=0;
+                j=0;
             }
             if(i<10)printf(" ");
-        	    printf("%d   ",i);
+            printf("%d   ",i);
         }
 }
 
+
+
+
+
+
+
+
+
+
 //==============================Funçoes de evento===================================================
+
+
 typedef struct {
     int hora;
     int min;
@@ -217,9 +238,14 @@ typedef struct {
     int dias_semana;
 }t_evento;
 
+
+
 int valida_hora(t_hora hora){
     return ( (hora.hora < 24 && hora.hora >= 0 ) && (hora.min >= 0 && hora.min < 60) );
 }
+
+
+
 
 void cria_evento (t_evento *evento ,time_t inicio,time_t fim, t_hora hora_inicio , t_hora hora_fim ,char * nome , char * local ,int recorrente, int dias){
     evento->inicio = inicio;
@@ -253,53 +279,77 @@ void modifica_evento (t_evento *evento ,time_t inicio,time_t fim, t_hora hora_in
     evento->dias_semana;
 }
 
+void ordena_data(t_evento * eventos, time_t inicio, int tamanho){
+    int i;
+    for(i = 0; i < ; i++){
+        for(j = i+1; j < tamanho; j++){
+            if(eventos[i]->inicio > eventos[j]->inicio){
+                int aux = eventos[j]->inicio;
+                eventos[j]->inicio = eventos[i]->inicio;
+                eventos[i]->inicio = aux;
+            }
+        }
+    }
+}
+
+int busca_data(t_evento * eventos, time_t inicio, int tamanho){
+    int e, d, meio;
+    e = 0; d = tamanho-1;
+    while(e <= d){
+        meio = (e+d)/2;
+        if(eventos[meio]->inicio == inicio){
+                return meio;
+        }
+        if (inicio > eventos[meio]->inicio){
+            e = meio+1;
+        }
+        else{
+            d = meio-1;
+        }
+
+    }
+    return meio;
+}
+
 void mfgets(char * texto, int tam){
     fgets(texto, tam, stdin);
     setbuf(stdin, NULL);
 }
 
-//Função Ordena por nome do evento.
-void InserSortNome(t_evento *name) {
-    int i, j;
-    t_evento aux;
-    for(i = 1; i < strlen(name) ; i++) {
-        aux = name[i];
-        for(j = i; (j>0) && (strcmp(aux.nome, name[j-1].nome) < 0); j--)
-            name[j] = name[j-1];
-        name[j] = aux;
-    }
-}
 
 //int compara_datas(tm data1, tm data_2){
 
 //}
 
+
+
+
 //==================== FUNÇOES DE ARQUIVO =====================
+
 void grava_eventos(t_evento * eventos){
     FILE * fpt;
     fpt = fopen("eventos.txt", "w");
     while (eventos->inicio!=-10){
-        fprintf(fpt, "%lu %lu %d %d %d %d %d %d %s%s", eventos->inicio, eventos->fim, eventos->hora_inicio.hora, eventos->hora_inicio.min, eventos->hora_fim.hora,
-        		 eventos->hora_fim.min , eventos->recorrente , eventos->dias_semana , eventos->nome , eventos->local);
+        fprintf(fpt , "%lu %lu %d %d %d %d %d %d %s%s" , eventos->inicio , eventos->fim , eventos->hora_inicio.hora , eventos->hora_inicio.min , eventos->hora_fim.hora , eventos->hora_fim.min , eventos->recorrente , eventos->dias_semana , eventos->nome , eventos->local);
         eventos = &eventos[1];
     }
     fclose(fpt);
 }
 
 int carrega_eventos(t_evento** eventos){
-    int res = 6, elem = 0, espaco = 100;
-    time_t inicio, fim ;
+    int res=6, elem = 0 , espaco=100;
+    time_t inicio , fim ;
     t_hora hora_inicio;
     t_hora hora_fim;
-    t_evento * eventoA = *eventos;
-    int recorrente, dias_semana;
-    char nome[81], local[81];
-    FILE *fpt;
+    t_evento * eventoA = * eventos;
+    int recorrente , dias_semana;
+    char  nome [81] , local [81];
+    FILE * fpt;
     fpt = fopen("eventos.txt", "r");
     eventoA = malloc(100 * sizeof(t_evento));
-    while(res == 6){
-        res = fscanf(fpt, "%lu %lu %d %d %d %d %[^\n] \r %[^\n]s", &inicio, &fim, &hora_inicio, &hora_fim, &recorrente, &dias_semana, nome, local);
-        cria_evento(&eventoA[elem], inicio, fim, hora_inicio, hora_fim, nome, local, recorrente, dias_semana);
+    while(res==6){
+        res = fscanf(fpt , "%lu %lu %d %d %d %d %[^\n] \r %[^\n]s" , &inicio , &fim , &hora_inicio , &hora_fim , &recorrente , &dias_semana , nome , local);
+        cria_evento(&eventoA[elem] , inicio , fim , hora_inicio , hora_fim , nome , local , recorrente , dias_semana );
         //printf("%s", *eventos[elem]->nome);
         elem++;
         espaco--;
@@ -307,7 +357,7 @@ int carrega_eventos(t_evento** eventos){
         printf("*");
         if(espaco == 0){
             espaco = 100;
-            realloc(eventoA, (/*elem*/ + 100) * sizeof(t_evento));
+            realloc(eventoA, (/elem/ + 100) * sizeof(t_evento));
         }
     }
     fclose(fpt);
@@ -315,25 +365,29 @@ int carrega_eventos(t_evento** eventos){
     return elem;
 }
 
+
+
+
+
 int main(){
     FILE *arquivo;
     t_evento eventos[11];
     t_evento  *eventos2;
-    time_t tmp;
+    time_t seila;
     int i=0, * b;
     char nome[50], local[50];
-    for(; i < 3; i++){
+    for(;i<3;i++){
         mfgets(nome, 50);
         mfgets(local, 50);
-        time(&tmp);
-        cria_evento(&eventos[i], tmp, tmp, (t_hora){10, 10}, (t_hora){10, 10}, nome, local, 123, 1324);
+        time(&seila);
+        cria_evento(&eventos[i] , seila , seila , (t_hora){10,10}, (t_hora){10,10} , nome , local , 123 , 1324);
     }
     eventos[i].inicio = -10;
     grava_eventos(eventos);
-    printf("####");
-    system("pause");
+            printf("####");
+            system("pause");
     carrega_eventos(&eventos2);
-	system("cls");
+system("cls");
     tm dataa;
     time_t teste1;
     dataa.tm_year = 2017;
@@ -343,8 +397,8 @@ int main(){
     dataa.tm_min = 20;
 
     time(&teste1);
-   	setlocale(LC_ALL, "");
-	printf("%ls",L"╔═════════════════════╤════════════════════════╦═══════════════════════════════════════════════════════╗\n║       DEZEMBRO      │         2017           ║                 QUARTA-FEIRA 8                        ║\n╠═════════════════════╧════════════════════════╬═══════════════════════════════════════════════════════╣\n║                                              ║                                                       ║\n║                                              ║                                                       ║\n║                                              ╟                                                       ╢\n║                                              ║                                                       ║\n║                                              ║                                                       ║\n║                                              ╟                                                       ╢\n║                                              ║                                                       ║\n║                                              ║                                                       ║\n║                                              ╟                                                       ╢\n║                                              ║                                                       ║\n║                                              ║                                                       ║\n║                                              ╟                                                       ╢\n║                                              ║                                                       ║\n║                                              ║                                                       ║\n║                                              ║                                                       ║\n║                                              ║                                                       ║\n║                                              ║                                                       ║\n╠══════════════════════════════════════════════║                                                       ║\n║                                              ║                                                       ║\n║ Inicio:                                      ║                                                       ║\n║ Fim:                                         ║                                                       ║\n║ Local:                                       ║                                                       ║\n║                                              ║                                                       ║\n║ Recorrente:                                  ║                                                       ║\n║    De:   /  /     até   /  /                 ║                                                       ║\n║                                              ║                                                       ║\n║                                              ║                                                       ║\n║                                              ║                                                       ║\n╚══════════════════════════════════════════════╩═══════════════════════════════════════════════════════╝\n");
+   setlocale(LC_ALL, "");
+printf("%ls",L"╔═════════════════════╤════════════════════════╦═══════════════════════════════════════════════════════╗\n║       DEZEMBRO      │         2017           ║                 QUARTA-FEIRA 8                        ║\n╠═════════════════════╧════════════════════════╬═══════════════════════════════════════════════════════╣\n║                                              ║                                                       ║\n║                                              ║                                                       ║\n║                                              ╟                                                       ╢\n║                                              ║                                                       ║\n║                                              ║                                                       ║\n║                                              ╟                                                       ╢\n║                                              ║                                                       ║\n║                                              ║                                                       ║\n║                                              ╟                                                       ╢\n║                                              ║                                                       ║\n║                                              ║                                                       ║\n║                                              ╟                                                       ╢\n║                                              ║                                                       ║\n║                                              ║                                                       ║\n║                                              ║                                                       ║\n║                                              ║                                                       ║\n║                                              ║                                                       ║\n╠══════════════════════════════════════════════║                                                       ║\n║                                              ║                                                       ║\n║ Inicio:                                      ║                                                       ║\n║ Fim:                                         ║                                                       ║\n║ Local:                                       ║                                                       ║\n║                                              ║                                                       ║\n║ Recorrente:                                  ║                                                       ║\n║    De:   /  /     até   /  /                 ║                                                       ║\n║                                              ║                                                       ║\n║                                              ║                                                       ║\n║                                              ║                                                       ║\n╚══════════════════════════════════════════════╩═══════════════════════════════════════════════════════╝\n");
 
     int a;
     mCursor cursor1;
